d6d107597d53fea2801139fe5ad75e3094462d82
Blanca MB <BMB@mbp-de-blanca.home>
2020-03-09 13:41:04 +0000

    created me logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+let cardOne = "king";
+let cardTwo = "queen";
+let cardThree = "queen";
+let cardFour = "king";
+
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardFour);
