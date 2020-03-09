3e6d5e7a11b74c9dfb5c227fd150205e79fec0b3
Blanca MB <BMB@mbp-de-blanca.home>
2020-03-09 20:06:51 +0000

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,21 @@
+const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push('cardOne');
+
+console.log("User flipped queen");
+
+let cardTwo = cards[2];
+cardsInPlay.push("cardTwo");
+
+console.log("User flipped a king");
+
+if (cardsInPlay.length === 2) {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	}
+	else {
+	alert("Sorry, try again.")
+	}
+}
