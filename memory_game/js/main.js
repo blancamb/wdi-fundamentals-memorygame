b7373e99e91095a3ce8f5c1731f5506283b62e3f
Blanca MB <BMB@mbp-de-blanca.home>
2020-03-11 18:54:17 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -13,7 +13,7 @@ const cards = [
 {
 	rank: "king",
 	suit: "hearts",
-	cardImage: "images/ling-of-hearts.png"
+	cardImage: "images/king-of-hearts.png"
 },
 {
 	rank: "king",
@@ -21,6 +21,7 @@ const cards = [
 	cardImage: "images/king-of-diamonds.png"
 }
 ];
+
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -32,20 +33,30 @@ function checkForMatch() {
 	console.log("Sorry, try again.");
 		alert("Sorry, try again.");
 	}
-}
+};
 
-function flipCard(cardId) {
-
+function flipCard() {
+	const cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank)
 	console.log(cards[cardId].cardImage)
 	console.log(cards[cardId].suit)
 
+	this.setAttribute('src', cards[cardId].cardImage);
+
 if (cardsInPlay.length === 2) {
 	checkForMatch();
-}"lo"
 }
+};
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
 
+createBoard();
